786dd92ef4ba2492f2f52efc03287acd3466c529
陳思淵 <chenszyuan@114-34-50-149.hinet-ip.hinet.net>
2026-02-01 21:43:42 +0800

    更新Day2行程-加入浄蓮の滝

---
+++ update-day2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sed -i '' 's/10:00<\/div><div class="event"><h3>⛩️ 修禪寺/09:30<\/div><div class="event"><h3>🌊 浄蓮の滝/g' index.html
+sed -i '' 's/本堂參拜，平地好走/伊豆名瀑，從旅館開車10分鐘/g' index.html
+sed -i '' 's/修禪寺" target="blank">導航/浄蓮の滝" target="blank">導航/g' index.html
+sed -i '' 's/11:00<\/div><div class="event"><h3>🎋 竹林小徑<\/h3><p>平緩步道，5-10 分鐘/11:00<\/div><div class="event"><h3>⛩️ 修禪寺<\/h3><p>本堂參拜，車程20分鐘/g' index.html
+sed -i '' 's/11:30<\/div><div class="event"><h3>♨️ 獨鈷之湯/11:45<\/div><div class="event"><h3>🎋 竹林小徑/g' index.html
+sed -i '' 's/河邊足湯，可休息/平緩步道，旁邊有足湯/g' index.html
